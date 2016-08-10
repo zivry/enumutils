4f69c335c66a73c7b269171a5c3511fa8dffac7a
Ivry, Zamir <zamir.ivry@intel.com>
2016-08-10 11:47:45 +0300

    First Commit

---
+++ INamedEnum.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.intel.swiss.sws.mechanism.enumutil;
+
+/**
+ * 
+ * This interface should be implemented by enumerations which holds a name variable.
+ * for each member.
+ * @author zivry
+ *
+ */
+public interface INamedEnum
+{
+	String getName();
+}
